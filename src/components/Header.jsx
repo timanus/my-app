c52a2af65d6068e8ca9033155aa764b82a1ceaa4
timanus <ulezko.2015@mail.ru>
2020-08-25 20:03:47 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://teinon.net/ficbook/uploads/avatars/avatar_By_VeroNik_1495405740.png" alt="" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src="http://wallpaperswebs.com/wp-content/uploads/Chinese-Lantern-Festival-1024x576.jpg" alt="" />
+			</div>
+			<div>
+				ava + description
+	</div>
+			<div>
+				My posts
+	  <div>
+					New posts
+	  </div>
+				<div>
+					<div>post1</div>
+					<div>post2</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
